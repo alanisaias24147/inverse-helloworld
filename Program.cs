ac200c7381b1ad2f60ba3904e88b2c1ca92bb709
alanisaias24147 <78932267+alanisaias24147@users.noreply.github.com>
2021-03-03 02:24:49 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace inverse_helloworld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string[] caracter = {"h","e","l","l","o","w","o","r","l","d","!" };
+            Console.WriteLine(caracter[10]+caracter[9]+caracter[8]+caracter[7]+caracter[6]+ caracter[5]+ caracter[4]+ caracter[3]+ caracter[2]+ caracter[1]+ caracter[0]);
+           
+          
+        }
+    }
+}
